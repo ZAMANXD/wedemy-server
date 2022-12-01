f4bffef26a1217bba84ef6cdc131e20ba2741aa1
ZAMANXD <znsnlab@gmail.com>
2022-12-01 12:21:51 +0600

    server fine tuned

--- index.js
+++ index.js
@@ -15,7 +15,6 @@ app.use(express.json());
 
 // dbaccess
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.s0orzt0.mongodb.net/?retryWrites=true&w=majority`;
-// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
